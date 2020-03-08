16ce05bdc2bbb6cf032ede010c3d20909c5722b4
Fenrir <61887706+FenrirC3@users.noreply.github.com>
2020-03-08 21:39:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+ const Tabletop = require('tabletop');
+
+ var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1r1wkiPF5SF1_0x1sIcC9OXfomNEylT8a2U0RYkedHLU/edit#gid=1632900943';
+
 function init() {
-  Tabletop.init( {
-    key: 'https://docs.google.com/spreadsheets/d/1r1wkiPF5SF1_0x1sIcC9OXfomNEylT8a2U0RYkedHLU/pubhtml',
-    simpleSheet: true }
-  ).then(function(data, tabletop) { 
-    console.log(data)
-  })
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: false } )
+  }
+
+ function showInfo(data, tabletop) {
+  // do something with the data
+  console.log(JSON.stringify(data, null, 2));
 }
-window.addEventListener('DOMContentLoaded', init)
+
+//initialise and kickstart the whole thing.
+init()
